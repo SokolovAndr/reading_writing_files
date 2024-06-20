b5b9e015de422cb8395e0dd37fa86b5750482bda
SokolovAndr <korch45@yandex.ru>
2024-06-20 17:00:21 +0300

    Реализованы механизмы записи и чтения int и String в файлах.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(
@@ -15,24 +13,12 @@ void main() {
 }
 
 class CounterStorage {
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
-  }
-
   Future<File> get _localFile async {
     return File('C:/Users/andrei.sokolov/Desktop/counter.txt');
   }
 
-  Future<int> readCounter() async {
-    try {
-      final file = await _localFile;
-      final contents = await file.readAsString();
-      return int.parse(contents);
-    } catch (e) {
-      // If encountering an error, return 0
-      return 0;
-    }
+  Future<File> get _localFile2 async {
+    return File('C:/Users/andrei.sokolov/Desktop/string.txt');
   }
 
   Future<List<int>> readCounters() async {
@@ -40,23 +26,42 @@ class CounterStorage {
       final file = await _localFile;
       final contents = await file.readAsString();
       List<int> myList = json.decode(contents).cast<int>();
-      return  myList;
+      return myList;
     } catch (e) {
       // If encountering an error, return 0
       return [0];
     }
   }
 
-  Future<File> writeCounter(int counter) async {
-  final file = await _localFile;
-  return file.writeAsString('$counter');
-}
+  Future<List<String>> readStrings() async {
+    try {
+      final file = await _localFile2;
+      final contents = await file.readAsString();
+      List<String> myList = json.decode(contents).cast<String>();
+      List<String> result = myList.map((e) => '\"$e\"').toList();
+      return result;
+    } catch (e) {
+      debugPrint(e.toString());
+      return [];
+    }
+  }
 
-  Future<File> writeList(String text) async {
+  Future<File> writeListOfInt(String text) async {
     final file = await _localFile;
     return file.writeAsString(text);
   }
 
+  Future<File> writeString(String text) async {
+    final file2 = await _localFile2;
+    return file2.writeAsString(text);
+  }
+
+  void clearAll() async {
+    final file1 = await _localFile;
+    final file2 = await _localFile2;
+    file1.delete();
+    file2.delete();
+  }
 }
 
 class FlutterDemo extends StatefulWidget {
@@ -69,84 +74,118 @@ class FlutterDemo extends StatefulWidget {
 }
 
 class _FlutterDemoState extends State<FlutterDemo> {
-  int _counter = 1;
-  List<int> values = [];
+  final textContoller = TextEditingController();
+  int _counter = 0;
+  List<int> countersFromList = [];
+  List<String> stringFromList = [];
+  String stringForScreen = '';
 
   @override
   void initState() {
     super.initState();
-    /*widget.storage.readCounter().then((value) {
-      setState(() {
-        _counter = value;
-        //values.add(_counter);
-      });
-    });*/
+    debugPrint("Run initState");
     widget.storage.readCounters().then((values2) {
       setState(() {
-        values = values2;
+        _counter = 0;
+        countersFromList = values2;
         _counter = values2.last;
+      });
+    });
+    widget.storage.readStrings().then((stringForScreen2) {
+      setState(() {
+        stringFromList = stringForScreen2;
       });
     });
   }
 
   Future<File> _incrementCounter() {
     setState(() {
-      _counter++;     
-    });
-    values.add(_counter);
-    return widget.storage.writeList(values.toString());
-  }
-
-  /*Future<File> _incrementCounter() {
-    setState(() {
       _counter++;
     });
+    countersFromList.remove(0);
+    countersFromList.add(_counter);
+    return widget.storage.writeListOfInt(countersFromList.toString());
+  }
 
-    // Write the variable as a string to the file.
-    return widget.storage.writeCounter(_counter);
-  }*/
+  Future<File> _addString() {
+    setState(() {
+      final String myString = textContoller.text.toString();
+      stringFromList.add("\"$myString\"");
+      stringForScreen = stringFromList.toString();
+    });
+
+    return widget.storage.writeString(stringForScreen);
+  }
+
+  void actionFun() {
+    if (textContoller.text.isEmpty) {
+      return;
+    } else {
+      _incrementCounter();
+      _addString();
+      textContoller.clear();
+    }
+  }
+
+  void clearFun() {
+    setState(() {
+      _counter = 0;
+      countersFromList = [];
+      stringFromList = [];
+      stringForScreen = "";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    final textContoller = TextEditingController();
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Reading and Writing Files'),
+        title: const Text('Чтение и запись файлов'),
       ),
       body: Column(
         children: [
-
           Center(
             child: Text(
-              'Button tapped $_counter time${_counter == 1 ? '' : 's'}.',
+              'Вы нажали $_counter раз${_counter == 2 || _counter == 3 || _counter == 4 ? 'а' : ''}.',
             ),
           ),
           Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextFormField(
-                controller: textContoller,
-                textCapitalization:
-                    TextCapitalization.sentences, //текст с заглавной буквы
-                maxLines: 1,
-                decoration: const InputDecoration(
-                    hintText: 'Текст',
-                    labelText: 'Введите текст',
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 0.75,
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10.0),
-                        ))),
-              ),
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              controller: textContoller,
+              textCapitalization:
+                  TextCapitalization.sentences, //текст с заглавной буквы
+              maxLines: 1,
+              decoration: const InputDecoration(
+                  hintText: 'Текст',
+                  labelText: 'Введите текст',
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 0.75,
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ))),
             ),
+          ),
+          Text(_counter == 0 ? 'Нет значений' : countersFromList.toString()),
+          Text(stringFromList.isEmpty
+              ? 'Нет значений'
+              : stringFromList.toString()),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50),
+            child: ElevatedButton(
+                onPressed: () {
+                  widget.storage.clearAll();
+                  clearFun();
+                },
+                child: const Icon(Icons.delete)),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: actionFun,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
